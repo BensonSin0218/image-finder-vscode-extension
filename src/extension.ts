9868cd8053c08384d230f0484c923ae5936e595d
Benson Sin <sinbenson0218@gmail.com>
2025-03-31 22:22:50 +0800

    feat: Add open setting

--- src/extension.ts
+++ src/extension.ts
@@ -55,6 +55,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const refreshAllViewsCommand = vscode.commands.registerCommand(`${EXTENSION_ID}.refreshAllViews`, () => refreshAllViews([imageTreeItemsTreeDataProvider, imageDetailTreeItemsTreeDataProvider]));
 
+	const openSettingsCommand = vscode.commands.registerCommand(`${EXTENSION_ID}.openSettings`, () => vscode.commands.executeCommand('workbench.action.openSettings', 'image-explorer'));
+
 	const openImageCommand = vscode.commands.registerCommand(`${EXTENSION_ID}.openImage`, (resource: vscode.Uri) => vscode.commands.executeCommand('vscode.open', resource));
 
 	const revealInExplorerCommand = vscode.commands.registerCommand(`${EXTENSION_ID}.revealInExplorer`, () => revealInExplorer(imageDetailTreeItemsTreeDataProvider));
@@ -64,6 +66,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		refreshAllViewsCommand,
+		openSettingsCommand,
 		openImageCommand,
 		revealInExplorerCommand,
 		findSimilarImagesCommand,
